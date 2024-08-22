58e410f44ca2726498c11761384f77e6cd380de7
takata-kento <takata_kento@ims-sol.co.jp>
2024-08-22 23:44:01 +0900

    generateStaticParamsによる複数ページの生成サンプル

---
+++ my-app/src/app/ssg/ssg-generateStaticParams-test/[id]/page.tsx
@@ -0,0 +1,42 @@
+import dynamic from "next/dynamic";
+import Head from "next/head";
+
+export function generateStaticParams() {
+    return [
+        {
+            id: '1'
+        },
+        {
+            id: '2'
+        },
+        {
+            id: '3'
+        }
+    ]
+}
+
+// 以下のRoute Segment ConfigをfalseにするとgenerateStaticParamsで生成されなかった動的なSegmentにアクセスしたときに404を返すようになる
+// デフォルトはtrue
+// export const dynamicParams = false
+
+// 以下はページアクセス時にレンダリングした時刻を表示する
+// dynamicParamsがtrueの場合、idに1/2/3のいずれかとそれ以外で時刻が違うことがわかる
+// 1or2or3はビルド時に静的にルートを生成していてそれ以外は動的に生成していることがわかる
+export default function Page({ params }: { params: { id: string } }) {
+    const { id } = params
+    const timestamp = new Date().toLocaleString()
+
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="../../favicon.ico" />
+            </Head>
+            <main>
+                <p>このページは静的サイト生成によってビルド時に生成されたページです</p>
+                <p>{`/posts/${id}に対応するページです`}</p>
+                <p>{`${timestamp}に作成されたページです`}</p>
+            </main>
+        </div>
+    )
+}
